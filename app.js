7aab4650bba28369a00e9cd6fc19943dc68b2eec
mirsv <mirsvobo@gmail.com>
2024-07-02 23:43:48 +0200

    Fungující přihlášení

--- app.js
+++ app.js
@@ -1,65 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const sequelize = require('./config/database');
-const logger = require('./config/logger');
-const isAuth = require('./middleware/isAuth');
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
+const User = require('./models/user');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(cookieParser());
 
-app.use((req, res, next) => {
-    logger.info(`Incoming request: ${req.method} ${req.url}`);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.locals.user = req.user || null;
+app.use(async (req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+        try {
+            const decodedToken = jwt.verify(token, 'your_secret_key');
+            const user = await User.findByPk(decodedToken.userId);
+            req.user = user;
+        } catch (err) {
+            console.log(err);
+        }
+    }
     next();
 });
 
 app.use('/auth', authRoutes);
-app.use('/posts', isAuth, postRoutes);
-app.use('/comments', isAuth, commentRoutes);
-app.use('/users', isAuth, userRoutes);
-app.use('/messages', isAuth, messageRoutes);
+app.use('/posts', postRoutes);
+app.use('/comments', commentRoutes);
 
-app.get('/', isAuth, (req, res) => { // Ochráníme i kořenovou trasu
-    res.redirect('/posts');
+app.get('/', (req, res, next) => {
+    if (req.user) {
+        return res.redirect('/posts');
+    }
+    res.redirect('/auth/login');
 });
 
-// Import models to ensure they are defined and associated properly
-const User = require('./models/user');
-const Post = require('./models/post');
-const Comment = require('./models/comment');
-const Like = require('./models/like');
-const University = require('./models/university');
-const StudyField = require('./models/studyField');
-const Dorm = require('./models/dorm');
-const Message = require('./models/message');
-
-sequelize.sync({ force: false }) // force: false to avoid dropping tables
-    .then(result => {
-        app.listen(3000, () => {
-            logger.info('Server is running on port 3000');
-        });
-    })
-    .catch(err => {
-        logger.error('Unable to connect to the database:', err);
-    });
-
-app.use((err, req, res, next) => {
-    logger.error('Something went wrong:', err);
-    res.status(500).json({ message: 'Internal server error' });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
